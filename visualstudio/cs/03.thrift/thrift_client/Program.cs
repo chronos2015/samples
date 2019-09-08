9e494725fa25e22a6fac88cc529efb2fa901fa15
chronos <tnomura217@gmail.com>
2019-09-09 00:27:25 +0900

    Thriftによる外部連携のサンプルをコミット

---
+++ visualstudio/cs/03.thrift/thrift_client/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Thrift.Protocol;
+using Thrift.Transport;
+
+namespace thrift_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if SOCKET
+            TTransport transport = new TSocket("localhost", 8080);
+#else
+            TTransport transport = new TNamedPipeClientTransport("MyPipeName");
+#endif
+            TProtocol protocol = new TBinaryProtocol(transport);
+            HelloWorld.Client client = new HelloWorld.Client(protocol);
+
+            transport.Open();
+            String res = client.hoge("Send to Server");
+            Console.WriteLine(res);
+        }
+    }
+}
