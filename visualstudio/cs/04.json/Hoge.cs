8d03e9df616534d6ee722b28465f4db5d0d57fce
chronos <tnomura217@gmail.com>
2019-09-09 20:59:34 +0900

    jconのシリアライズとデシリアライズのサンプルを追加

---
+++ visualstudio/cs/04.json/Hoge.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace json
+{
+    public class Hoge
+    {
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "ags")]
+        public int Age { get; set; }
+    }
+}
