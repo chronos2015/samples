7bf5d9bfb1a8b3a4669aa49267fd3c9278292d69
chronos <tnomura217@gmail.com>
2019-09-19 03:20:26 +0900

    evosuiteによる単体テストのサンプルを追加

---
+++ java/codes/04.EvoSuite/src/main/java/org/example/helloWorld/Program.java
@@ -0,0 +1,11 @@
+package org.example.helloWorld;
+
+public class Program {
+    public static void main(String agrs[]) {
+        System.out.println("hello, World!");
+    }
+
+    public static boolean isEvenNumber(int test) {
+        return test % 2 == 0;
+    }
+}
