d1e16b2be734446e9ea5c8be9d70987f6f4bca7a
chronos <tnomura217@gmail.com>
2019-09-29 23:45:22 +0900

    C# LINQのサンプルを追加

---
+++ visualstudio/cs/07.Linq/Linq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> data = new List<string>() { "1", "2", "2", "3", "3" };
+            string ret = data.GroupBy(s => s)
+                .Select(s => new { key = s.First(), size = s.Count() })
+                .OrderByDescending(s => s.size)
+                .ThenByDescending(s => s.key)
+                .First().key;
+        }
+    }
+}
