6dbbe9506c815658f9739a97764fc10720e3c14a
chronos <tnomura217@gmail.com>
2019-10-01 07:37:25 +0900

    テストコードの追加

--- java/codes/05.EvoSuite/src/main/java/org/example/helloWorld/Program.java
+++ java/codes/05.EvoSuite/src/main/java/org/example/helloWorld/Program.java
@@ -1,5 +1,8 @@
 package org.example.helloWorld;
 
+import java.time.LocalDateTime;
+import java.util.Calendar;
+
 public class Program {
     public static void main(String agrs[]) {
         System.out.println("hello, World!");
@@ -7,5 +10,22 @@ public class Program {
 
     public static boolean isEvenNumber(int test) {
         return test % 2 == 0;
+    }
+
+    public static int getYear() {
+        Calendar calendar = Calendar.getInstance();
+        return calendar.get(Calendar.YEAR);
+    }
+
+    public static int getAccountYear() {
+        LocalDateTime d = LocalDateTime.now();
+        if (d.getMonth().getValue() < 4) {
+            return d.getYear() -1;
+        }
+        return d.getYear();
+    }
+
+    public static int getParent() {
+        return getParent();
     }
 }
