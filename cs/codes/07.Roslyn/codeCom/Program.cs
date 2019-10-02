0943c6d93a4da3f293724a99ea8ddaafb90dcde5
chronos <tnomura217@gmail.com>
2019-10-02 20:47:53 +0900

    C#,roslynのサンプルを追加

---
+++ cs/codes/07.Roslyn/codeCom/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace CodeDom
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            // static void Main(string[] args) {
+            //     Console.WriteLine("hello,world!");
+            // }
+            var methodDesc = SyntaxFactory
+                .MethodDeclaration(SyntaxFactory.ParseTypeName("void"), "Main")
+                .AddModifiers(SyntaxFactory.Token(SyntaxKind.StaticKeyword))
+                .AddParameterListParameters(SyntaxFactory.Parameter(SyntaxFactory.Identifier("args")).WithType(SyntaxFactory.ParseTypeName(typeof(string[]).FullName)))
+                .AddBodyStatements(SyntaxFactory.ParseStatement("Console.WriteLine(\"hello,world!\");"));
+
+            // public class Program : Object
+            var classDesc = SyntaxFactory
+                .ClassDeclaration("Program")
+                .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
+                .AddBaseListTypes(SyntaxFactory.SimpleBaseType(SyntaxFactory.ParseTypeName("Object")))
+                .AddMembers(methodDesc);
+
+            // namespace sample {}
+            var nameapace = SyntaxFactory
+                .NamespaceDeclaration(SyntaxFactory.ParseName(" sample"))
+                .AddUsings(SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("System")))
+                .AddMembers(classDesc);
+
+            var code = nameapace.NormalizeWhitespace().ToFullString();
+
+            // Output new code to the console.
+            Console.WriteLine(code);
+        }
+    }
+}
