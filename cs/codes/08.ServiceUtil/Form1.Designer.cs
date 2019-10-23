333cbc0bf94142548919df08820fc9e370552be7
chronos <tnomura217@gmail.com>
2019-10-24 06:14:07 +0900

    簡単なユーティリティを追加

---
+++ cs/codes/08.ServiceUtil/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace ServiceUtil
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BtnTomcat = new System.Windows.Forms.Button();
+            this.BtnTeamCity = new System.Windows.Forms.Button();
+            this.BtnTCBuildAgent = new System.Windows.Forms.Button();
+            this.BtnSonarQube = new System.Windows.Forms.Button();
+            this.Btnupsource = new System.Windows.Forms.Button();
+            this.BtnJenkins = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.BtnJava = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.BtnRestart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // BtnTomcat
+            // 
+            this.BtnTomcat.AutoSize = true;
+            this.BtnTomcat.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnTomcat.Location = new System.Drawing.Point(12, 53);
+            this.BtnTomcat.Name = "BtnTomcat";
+            this.BtnTomcat.Size = new System.Drawing.Size(142, 35);
+            this.BtnTomcat.TabIndex = 0;
+            this.BtnTomcat.Text = "Tomcat";
+            this.BtnTomcat.UseVisualStyleBackColor = true;
+            this.BtnTomcat.Click += new System.EventHandler(this.BtnTomcat_Click);
+            // 
+            // BtnTeamCity
+            // 
+            this.BtnTeamCity.AutoSize = true;
+            this.BtnTeamCity.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnTeamCity.Location = new System.Drawing.Point(160, 53);
+            this.BtnTeamCity.Name = "BtnTeamCity";
+            this.BtnTeamCity.Size = new System.Drawing.Size(142, 35);
+            this.BtnTeamCity.TabIndex = 1;
+            this.BtnTeamCity.Text = "TeamCity";
+            this.BtnTeamCity.UseVisualStyleBackColor = true;
+            this.BtnTeamCity.Click += new System.EventHandler(this.BtnTeamCity_Click);
+            // 
+            // BtnTCBuildAgent
+            // 
+            this.BtnTCBuildAgent.AutoSize = true;
+            this.BtnTCBuildAgent.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnTCBuildAgent.Location = new System.Drawing.Point(308, 53);
+            this.BtnTCBuildAgent.Name = "BtnTCBuildAgent";
+            this.BtnTCBuildAgent.Size = new System.Drawing.Size(142, 35);
+            this.BtnTCBuildAgent.TabIndex = 2;
+            this.BtnTCBuildAgent.Text = "TCBuildAgent";
+            this.BtnTCBuildAgent.UseVisualStyleBackColor = true;
+            this.BtnTCBuildAgent.Click += new System.EventHandler(this.BtnTCBuildAgent_Click);
+            // 
+            // BtnSonarQube
+            // 
+            this.BtnSonarQube.AutoSize = true;
+            this.BtnSonarQube.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSonarQube.Location = new System.Drawing.Point(456, 53);
+            this.BtnSonarQube.Name = "BtnSonarQube";
+            this.BtnSonarQube.Size = new System.Drawing.Size(142, 35);
+            this.BtnSonarQube.TabIndex = 3;
+            this.BtnSonarQube.Text = "SonarQube";
+            this.BtnSonarQube.UseVisualStyleBackColor = true;
+            this.BtnSonarQube.Click += new System.EventHandler(this.BtnSonarQube_Click);
+            // 
+            // Btnupsource
+            // 
+            this.Btnupsource.AutoSize = true;
+            this.Btnupsource.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btnupsource.Location = new System.Drawing.Point(604, 53);
+            this.Btnupsource.Name = "Btnupsource";
+            this.Btnupsource.Size = new System.Drawing.Size(142, 35);
+            this.Btnupsource.TabIndex = 4;
+            this.Btnupsource.Text = "Upsource";
+            this.Btnupsource.UseVisualStyleBackColor = true;
+            this.Btnupsource.Click += new System.EventHandler(this.Btnupsource_Click);
+            // 
+            // BtnJenkins
+            // 
+            this.BtnJenkins.AutoSize = true;
+            this.BtnJenkins.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnJenkins.Location = new System.Drawing.Point(160, 12);
+            this.BtnJenkins.Name = "BtnJenkins";
+            this.BtnJenkins.Size = new System.Drawing.Size(142, 35);
+            this.BtnJenkins.TabIndex = 5;
+            this.BtnJenkins.Text = "Jenkins";
+            this.BtnJenkins.UseVisualStyleBackColor = true;
+            this.BtnJenkins.Click += new System.EventHandler(this.BtnJenkins_Click);
+            // 
+            // button6
+            // 
+            this.button6.AutoSize = true;
+            this.button6.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(308, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(142, 35);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Jetty";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.AutoSize = true;
+            this.button7.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(456, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(142, 35);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "GlassFish";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // BtnJava
+            // 
+            this.BtnJava.AutoSize = true;
+            this.BtnJava.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnJava.Location = new System.Drawing.Point(12, 12);
+            this.BtnJava.Name = "BtnJava";
+            this.BtnJava.Size = new System.Drawing.Size(142, 35);
+            this.BtnJava.TabIndex = 8;
+            this.BtnJava.Text = "java";
+            this.BtnJava.UseVisualStyleBackColor = true;
+            this.BtnJava.Click += new System.EventHandler(this.BtnJava_Click);
+            // 
+            // button9
+            // 
+            this.button9.AutoSize = true;
+            this.button9.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(604, 12);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(142, 35);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "WildFly";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.AutoSize = true;
+            this.button10.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(752, 12);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(142, 35);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Ngix";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // BtnRestart
+            // 
+            this.BtnRestart.AutoSize = true;
+            this.BtnRestart.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnRestart.Location = new System.Drawing.Point(752, 53);
+            this.BtnRestart.Name = "BtnRestart";
+            this.BtnRestart.Size = new System.Drawing.Size(142, 35);
+            this.BtnRestart.TabIndex = 11;
+            this.BtnRestart.Text = "Restart";
+            this.BtnRestart.UseVisualStyleBackColor = true;
+            this.BtnRestart.Click += new System.EventHandler(this.BtnRestart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 96);
+            this.Controls.Add(this.BtnRestart);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.BtnJava);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.BtnJenkins);
+            this.Controls.Add(this.Btnupsource);
+            this.Controls.Add(this.BtnSonarQube);
+            this.Controls.Add(this.BtnTCBuildAgent);
+            this.Controls.Add(this.BtnTeamCity);
+            this.Controls.Add(this.BtnTomcat);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "サービスユーティリティ";
+            this.TopMost = true;
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnTomcat;
+        private System.Windows.Forms.Button BtnTeamCity;
+        private System.Windows.Forms.Button BtnTCBuildAgent;
+        private System.Windows.Forms.Button BtnSonarQube;
+        private System.Windows.Forms.Button Btnupsource;
+        private System.Windows.Forms.Button BtnJenkins;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button BtnJava;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button BtnRestart;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ cs/codes/08.ServiceUtil/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServiceUtil
+{
+    public partial class Form1 : Form
+    {
+        private const string TEAM_CITY = "TeamCity";
+        private const string TC_BUILD_AGENT = "TCBuildAgent";
+        private const string SONAQ_QUBE = "SonarQube";
+        private const string UPSOURCE = "upsource";
+        private const string TOMCAT = "tomcat";
+        private const string JENKINS = "jenkins";
+
+        public Form1()
+        {
+            InitializeComponent();
+            Point location = this.Location;
+            location.X = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width - this.Width - 30;
+            location.Y = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height - this.Height - 100;
+            this.Location = location;
+        }
+
+        private static void SetColor(Button target, IEnumerable<ServiceController> query)
+        {
+            target.Enabled = query.Any();
+            if (target.Enabled)
+            {
+                switch (query.First().Status)
+                {
+                    case ServiceControllerStatus.Running:
+                        target.ForeColor = Color.DarkBlue;
+                        break;
+                    case ServiceControllerStatus.Stopped:
+                        target.ForeColor = Color.Red;
+                        break;
+                    default:
+                        target.ForeColor = Color.Green;
+                        break;
+                }
+            }
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private static IEnumerable<ServiceController> GetService(ServiceController[] services, string name)
+        {
+            return from i in services where i.ServiceName == name select i;
+        }
+
+        private static void ControllService(string name)
+        {
+            ServiceController[] services = ServiceController.GetServices();
+            var service = GetService(services, name).First();
+            if (service.Status == ServiceControllerStatus.Stopped)
+            {
+                service.Start();
+            }
+            else if (service.Status == ServiceControllerStatus.Running)
+            {
+                service.Stop();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ServiceController[] services = ServiceController.GetServices();
+
+            int size = Process.GetProcessesByName("java").Length + Process.GetProcessesByName("javaw").Length;
+            BtnJava.Enabled = size != 0;
+            BtnJava.Text = string.Format("java({0})", size);
+
+            SetColor(button6, from i in services where i.ServiceName == "jetty" select i);
+            SetColor(BtnTomcat, GetService(services, TOMCAT));
+            SetColor(button7, from i in services where i.ServiceName == "glassfish" select i);
+            SetColor(button9, from i in services where i.ServiceName == "wildfly" select i);
+            SetColor(button10, from i in services where i.ServiceName == "ngix" select i);
+
+            SetColor(BtnJenkins, GetService(services, JENKINS));
+            SetColor(BtnTeamCity, GetService(services, TEAM_CITY));
+            SetColor(BtnTCBuildAgent, GetService(services, TC_BUILD_AGENT));
+            SetColor(BtnSonarQube, GetService(services, SONAQ_QUBE));
+            SetColor(Btnupsource, GetService(services, UPSOURCE));
+        }
+
+        private void BtnTeamCity_Click(object sender, EventArgs e)
+        {
+            ControllService(TEAM_CITY);
+        }
+
+        private void BtnTCBuildAgent_Click(object sender, EventArgs e)
+        {
+            ControllService(TC_BUILD_AGENT);
+        }
+
+        private void BtnSonarQube_Click(object sender, EventArgs e)
+        {
+            ControllService(SONAQ_QUBE);
+        }
+
+        private void Btnupsource_Click(object sender, EventArgs e)
+        {
+            ControllService(UPSOURCE);
+        }
+
+        private void BtnTomcat_Click(object sender, EventArgs e)
+        {
+            ControllService(TOMCAT);
+        }
+
+        private void BtnRestart_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void BtnJava_Click(object sender, EventArgs e)
+        {
+            foreach(Process p in Process.GetProcessesByName("java"))
+            {
+                p.Kill();
+            }
+            foreach (Process p in Process.GetProcessesByName("javaw"))
+            {
+                p.Kill();
+            }
+        }
+
+        private void BtnJenkins_Click(object sender, EventArgs e)
+        {
+            ControllService(JENKINS);
+        }
+    }
+}
